ede17219c4e4629322ff1fb00db1af3b52c3394c
Haeun Baek <haeunbaek99@gmail.com>
2024-10-17 18:59:18 +0900

    1017

--- FAQ.py
+++ FAQ.py
@@ -5,32 +5,24 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: #4CAF50;'>FAQ</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center;'>전기차 충전에 관한 FAQ를 제공합니다.</h3>", unsafe_allow_html=True)
 
-tab1, tab2, tab3 = st.tabs(['KIA EV','GS CALTEX', 'TAGO EV'])
+tab1, tab2 = st.tabs(['KIA EV', 'TAGO EV'])
 
 with tab1: 
-# Load data
+    # Load data
     df = pd.read_csv('EV_faq3.csv', index_col=False)
-
 
     # Display FAQs in an accordion style
     for index, row in df.iterrows():
         with st.expander(f"**{row['Questions']}**"):
             st.write(f"{row['Answer']}", unsafe_allow_html=True)
 
-
 with tab2:
-    st.write('GS CALTEX FAQ content goes here')
-
-with tab3: 
     df = pd.read_csv('tago_faq.csv', index_col=False)
-
 
     # Display FAQs in an accordion style
     for index, row in df.iterrows():
         with st.expander(f"**{row['Question']}**"):
             st.write(f"{row['Answer']}", unsafe_allow_html=True)
-
-
 
 st.markdown(
     """
@@ -49,18 +41,3 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
